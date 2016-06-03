5002776f7e4e1aeac8a625e97b0afc9d3392e1a7
Diego Campos Aquino <caquinodiego@crece.uss.edu.pe>
2016-06-03 11:27:49 -0500

    butterknbife and constants

--- app/src/main/java/com/wargames/tipcalc/MainActivity.java
+++ app/src/main/java/com/wargames/tipcalc/MainActivity.java
@@ -2,36 +2,63 @@ package com.wargames.tipcalc;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    @Bind(R.id.inputBill)
+    EditText inputBill;
+    @Bind(R.id.btnSubmit)
+    Button btnSubmit;
+    @Bind(R.id.inputPercentage)
+    EditText inputPercentage;
+    @Bind(R.id.btnIncrease)
+    Button btnIncrease;
+    @Bind(R.id.btnDecrease)
+    Button btnDecrease;
+    @Bind(R.id.btnClear)
+    Button btnClear;
+    @Bind(R.id.txtTip)
+    TextView txtTip;
+
+    //definir constantes
+    private final static int TIP_STEP_CHANGE = 1;
+    private final static int DEFAULT_TIP_PERCENTAGE = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main,menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
     }
-     @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-         //obtener el identificador del item seleccionado
-         if ( item.getItemId() == R.id.action_about)
-         {
-             about();
-         }
-         return super.onOptionsItemSelected(item);
-     }
 
-    private void about()
-    {
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //obtener el identificador del item seleccionado
+        if (item.getItemId() == R.id.action_about) {
+            about();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void about() {
         TipCalcApp app = (TipCalcApp) getApplication();
         String strUrl = app.getAboutUrl();
 
